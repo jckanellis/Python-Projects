3e365f67f887f024528ea141a24a7fc830f229e3
johnkanellis <jckanellis889@gmail.com>
2022-02-10 22:00:53 -0800

    Add plot for pro bowls by round

--- nfl_draft_analysis/nfl_draft_analysis.py
+++ nfl_draft_analysis/nfl_draft_analysis.py
@@ -8,3 +8,15 @@ exec(open("draft_data_scraper.py").read())
 
 # Clean up player names
 df['Player'] = df['Player'].str.replace('HOF', '')
+
+# Convert Pro Bowl values to integers
+df['PB'] = pd.to_numeric(df['PB'])
+
+pro_bowls = df.groupby('Round')['PB'].sum()
+
+# Plot number of Pro Bowlers by draft round
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+round = np.arange(1, 8, 1).tolist()
+ax.bar(round, pro_bowls)
+plt.show()
